28e1f66e772c3c5815ab99a180a4e2e055250ee1
dongtran <thiendong.iuh@gmail.com>
2023-09-30 23:43:29 +0700

    add foreground service for send message to telegram throught bot

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,13 +44,12 @@ android {
     }
     packaging {
         resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "/META-INF/{AL2.0,LGPL2.1,INDEX.LIST}"
         }
     }
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.0")
@@ -66,4 +65,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
--- app/src/main/java/com/dongtran/wirelesschargeoptimize/ForegroundService.kt
+++ app/src/main/java/com/dongtran/wirelesschargeoptimize/ForegroundService.kt
@@ -7,14 +7,26 @@ import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
 import android.os.Build
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
 import android.util.Log
 import androidx.annotation.RequiresApi
 import java.util.*
-
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 class ForegroundService : Service() {
     private val TAG = "ForegroundService"
     private val timer = Timer()
+    private val handler = Handler(Looper.getMainLooper())
+
 
     companion object {
         private const val NOTIFICATION_CHANNEL_ID = "ForegroundServiceChannel"
@@ -26,22 +38,48 @@ class ForegroundService : Service() {
         createNotificationChannel()
     }
 
+    private var isServiceRunning = false
+    private var cnt = 0
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.d(TAG, "Service started")
 
-        val notification = createNotification()
+        if (!isServiceRunning) {
+            val notification = createNotification()
 
-        // Bắt đầu dịch vụ Foreground với thông báo
-        startForeground(NOTIFICATION_ID, notification)
+            // Bắt đầu dịch vụ Foreground với thông báo
+            startForeground(NOTIFICATION_ID, notification)
+            isServiceRunning = true
+
+            handler.postDelayed(object : Runnable {
+                override fun run() {
+                    // Gửi API POST ở đây
+                    Log.d(TAG, "AAA POST request...")
+                    GlobalScope.launch(Dispatchers.IO) {
+                        sendTelegramMessage(cnt)
+                    }
+                    cnt++
+                    // Lên lịch tiếp theo sau 10 giây
+                    handler.postDelayed(this, 10000)
+                }
+            }, 10000)
+        }
 
         // Lập lịch gửi API POST mỗi 2 phút (120000 milliseconds)
-        timer.scheduleAtFixedRate(object : TimerTask() {
-            override fun run() {
-                // Gửi API POST ở đây
-                Log.d(TAG, "Sending POST request...")
-            }
-        }, 0, 60000) // 120000 milliseconds = 2 phút
+        //timer.scheduleAtFixedRate(object : TimerTask() {
+        //    override fun run() {
+        //        // Gửi API POST ở đây
+        //        Log.d(TAG, "Sending POST request...")
+        //        GlobalScope.launch(Dispatchers.IO) {
+        //            sendTelegramMessage(cnt)
+        //        }
+        //        cnt = cnt + 1
+        //    }
+        //}, 0, 10000) // 120000 milliseconds = 2 phút
+
+
+
 
         return START_STICKY
     }
@@ -85,4 +123,43 @@ class ForegroundService : Service() {
 
         return notification
     }
+
+    private fun sendTelegramMessage(@Field("cnt") cnt: Number,) {
+        val token = "" // Thay YOUR_BOT_TOKEN bằng token của bot của bạn
+        val chatId = "" // Thay YOUR_CHAT_ID bằng chat ID của người nhận
+        val message = "Hello from Kotlin! " + cnt.toString() // Nội dung tin nhắn của bạn
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.telegram.org/bot$token/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val telegramAPI = retrofit.create(TelegramAPI::class.java)
+
+        val call = telegramAPI.sendMessage(chatId, message)
+
+        try {
+            val response = call.execute()
+            if (response.isSuccessful) {
+                println("Tin nhắn đã được gửi thành công!")
+            } else {
+                val errorBody = response.errorBody()?.string()
+                println("Gửi tin nhắn thất bại: $errorBody")
+                //updateNotification("Failed to send message: $errorBody")
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            println("Gửi tin nhắn thất bại: ${e.message}")
+            //updateNotification("Failed to send message: ${e.message}")
+        }
+    }
+}
+
+interface TelegramAPI {
+    @FormUrlEncoded
+    @POST("sendMessage")
+    fun sendMessage(
+        @Field("chat_id") chatId: String,
+        @Field("text") text: String
+    ): Call<Unit>
 }
